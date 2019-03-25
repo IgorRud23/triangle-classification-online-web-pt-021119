ee48a606b078877d7fb5025273b638cb11c42c6b
Igor Rud <rud.igor23@gmail.com>
2019-03-25 20:30:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+attr_accessor :left, :right, :bottom
+
+def initialize(left, right, buttom)
+  @left = left
+  @right = right
+  @bottom = bottom
+end
+
+def kind
+end
+
+
+class TriangleError < StandardError
+  "Costom error of triangle"
+end
+
 end
